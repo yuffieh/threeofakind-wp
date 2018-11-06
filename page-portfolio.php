4d494c292a9a6480e06aa0088f21cff12e667134
Serah Huynh <yuffieh@Yuffiehs-Computer.local>
2018-11-06 00:01:01 -0700

    added page-portfolio

--- page-portfolio.php
+++ page-portfolio.php
@@ -12,29 +12,26 @@ if (is_front_page()) {
 } ?>
 
 
-<div class="container">
-    <h1 class="page-title">Meet The Board</h1>
+<div class="center-container">
+    <h1 class="page-title"><?php the_title(); ?></h1>
+    <div class="portfolio-container">
     <?php
     $count = 0;
     $args = array(
         'type' => 'post',
-        'posts_per_page' => -1,
-        'cat' => '3',
+        'posts_per_page' => 9,
+        'cat' => '5',
         'order_by' => 'date');
     ?>
     <?php query_posts($args); ?>
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
-            <div class="section-one">
-                <img class="main-img" src="<?php the_post_thumbnail_url('full'); ?>"/>
-                <div class="main-info">
-                    <h2 class="member-name"><?php the_title(); ?></h2>
-                    <br/>
-                    <?php the_content(); ?>
+                <div class="portfolio-img" style="background-image: url('<?php the_post_thumbnail_url('full'); ?>')">
                 </div>
-            </div>
+
         <?php endwhile; ?>
     <?php endif; ?>
+    </div>
 </div>
 
 
